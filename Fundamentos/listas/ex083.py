40d0186756a85fc440181833f0340a90cd499739
HenriqueLopes741 <henrique.lopes.d.m@gmail.com>
2026-01-30 11:48:25 -0300

    add: ex083

--- Fundamentos/listas/ex083.py
+++ Fundamentos/listas/ex083.py
@@ -1,1 +1,18 @@
-print('oi')
+expressao = str(input('Digite a expressão: '))
+
+lista = []
+
+for simb in expressao:
+    if simb == '(':
+        lista.append('(')
+    elif simb == ')':
+        if len(lista) > 0 :
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+
+if len(lista) == 0:
+    print('Sua expressao esta valida!')
+else:
+    print('Sua expressao esta invalida! ')
