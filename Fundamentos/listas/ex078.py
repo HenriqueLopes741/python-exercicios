9594867424672aebcc979e781ad6d0c49fbc13b4
HenriqueLopes741 <henrique.lopes.d.m@gmail.com>
2026-01-31 00:02:42 -0300

    add: ex078

--- Fundamentos/listas/ex078.py
+++ Fundamentos/listas/ex078.py
@@ -1,9 +1,9 @@
-valores = []
+val = []
 
 for v in range(0, 5):
     numero = int(input(f'Digite um valor para a Posição {v}: '))
-    valores.append(numero)
+    val.append(numero)
 
-print(f'Voce digitou os valores {valores}')
-print(f'O maior valor digitado foi {max(valores)} nas posições ')
-print(f'O menor valor digitado foi {min(valores)}')
+print(f'Voce digitou os valores {val}')
+print(f'O maior valor digitado foi {max(val)} nas posições ')
+print(f'O menor valor digitado foi {min(val)}')
