5511363cab073ed6f5e827e41496d3deff289c25
HenriqueLopes741 <henrique.lopes.d.m@gmail.com>
2026-02-03 17:05:12 -0300

    bot para subir todos os arquivos para o git

--- Fundamentos/listas/ex084.py
+++ Fundamentos/listas/ex084.py
@@ -1,1 +1,16 @@
-print('Ola mundo salvando')
+lista1 = []
+lista2 = []
+
+while True:
+    lista1.append(str(input('Nome: ')))
+    lista1.append(float(input('Peso: ')))
+    lista2.append((lista1[:]))
+    lista1.clear()
+
+    resposta = str(input(('Quer continuar? [S/N] ')))
+    if resposta in 'Nn':
+        break
+
+print('-=' * 30)
+print(f'Os dados foram {lista1} ')
+print(f'Ao todo, você cadastrou {len(lista2)} pessoas')
