eb8b20c003f66b7fe430fad1b4e9c1108c1ce403
HenriqueLopes741 <henrique.lopes.d.m@gmail.com>
2026-02-03 17:13:19 -0300

    03/02/2026

--- Fundamentos/listas/ex084.py
+++ Fundamentos/listas/ex084.py
@@ -1,9 +1,19 @@
 lista1 = []
 lista2 = []
 
+maior = menor = 0
+
 while True:
     lista1.append(str(input('Nome: ')))
     lista1.append(float(input('Peso: ')))
+    if len(lista2) == 0:
+        maior = menor = lista1[1]
+    else:
+        if lista1[1] > maior:
+            maior = lista1[1]
+        if lista1[1] < menor:
+            menor = lista1[1]
+
     lista2.append((lista1[:]))
     lista1.clear()
 
@@ -12,5 +22,7 @@ while True:
         break
 
 print('-=' * 30)
-print(f'Os dados foram {lista1} ')
+print(f'Os dados foram {lista2} ')
 print(f'Ao todo, você cadastrou {len(lista2)} pessoas')
+print(f'O maior peso foi de {maior} KG')
+print(f'O menor peso foi de {menor} KG')
