a6b4b8511a76fef54e02aed31bc016dbd2e9bd65
HenriqueLopes741 <henrique.lopes.d.m@gmail.com>
2026-02-03 17:49:57 -0300

    codigo finalizado analisando matrizes

--- Fundamentos/listas/ex087.py
+++ Fundamentos/listas/ex087.py
@@ -1,5 +1,6 @@
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0],]
 somas = []
+coluna = 0 
 
 for l in range(0, 3): 
     for c in range(0, 3):
@@ -17,3 +18,15 @@ for l in range(0, 3):
 print('-=' * 20)
 
 print(f'A Soma dos numeros pares é {sum(somas)}')
+
+for l in range(0, 3):
+    coluna += matriz[l][2]
+
+print(f'A soma dos valores da terceira coluna é {coluna}.')
+
+for c in range(0 ,3):
+    if c == 0:
+        maior = matriz[1][c]
+    elif matriz[1][c]:
+        maior = matriz[1][c]
+print(f'O maior valor da segunda linha é {maior}')
