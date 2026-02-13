1e5d57e9742fe3603e8a77a7fb03b286e679c143
HenriqueLopes741 <henrique.lopes.d.m@gmail.com>
2026-02-13 17:54:19 -0300

    13/02

---
+++ Fundamentos/funcoes/ex0100.py
@@ -0,0 +1,21 @@
+from random import randint
+num = list()
+
+
+def sortear(lista):
+    print('Sorteando 5 valores da lista: ')
+    for cont in range(1, 6):
+        lista.append(randint(1, 10))
+
+def somarPar(lista):
+    soma = 0 
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
+
+
+
+sortear(num)
+print(num)
+somarPar(num)
