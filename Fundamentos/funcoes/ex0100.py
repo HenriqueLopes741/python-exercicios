24218dead34bcc0362fe5a435b6530acb153a197
HenriqueLopes741 <henrique.lopes.d.m@gmail.com>
2026-02-14 22:00:38 -0300

    14/02

--- Fundamentos/funcoes/ex0100.py
+++ Fundamentos/funcoes/ex0100.py
@@ -12,7 +12,7 @@ def somarPar(lista):
     for valor in lista:
         if valor % 2 == 0:
             soma += valor
-    print(f'Somando os valores pares de {lista}, temos  somassss {soma}')
+    print(f'Somando os valores pares de {lista}, temos  somasssss {soma}')
 
 
 
