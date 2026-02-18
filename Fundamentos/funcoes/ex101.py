6f2b4ade86e576a416ce05b6084a9b130ef48969
Henrique Lopes <henrique.lopes.d.m@gmail.com>
2026-02-17 21:06:56 -0300

    17/02

--- Fundamentos/funcoes/ex101.py
+++ Fundamentos/funcoes/ex101.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 
-ano = datetime.now().year
+atual = datetime.now().year
 
-def alistamento(data):
-    data = int(input('Em que ano você nasceu? '))
-    idade = ano - data
+def voto(ano):
+    idade = atual - ano
 
     if idade < 16:
         return f'Com {idade} anos: NÃO VOTA'
     elif 16 <= idade < 18 or idade > 65:
-        return f'Com {idade} anos: VOTO OPCIONAL' 
+        return f'Com {idade} anos: VOTO OPCIONAL'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO'
+
+nas = int(input("Em que ano você nasceu? "))
+print(voto(nas))
