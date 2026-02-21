b63f5538066d88053a9e50f928ce409d2ff3f3b6
Henrique Lopes <henrique.lopes.d.m@gmail.com>
2026-02-20 21:35:17 -0300

    20/02

--- Fundamentos/Modulos/ex107.py
+++ Fundamentos/Modulos/ex107.py
@@ -1,5 +1,5 @@
 from Fundamentos.Modulos.funções import aumentar, metade, dobro
-a = int(input("Digite o preço: R$"))
+a = int(input("Digite o preço do produto: R$"))
 print('-=' * 20)
 print(f'A metade de R${a} é R${metade(a)} Reais')
 print(f'O dobro de R${a} é R${dobro(a)} reais')
