0c67b70b5dcb5dbd0b05277932c07c58c24e022d
Henrique Lopes <henrique.lopes.d.m@gmail.com>
2026-02-20 21:34:49 -0300

    20/02

--- Fundamentos/Modulos/ex107.py
+++ Fundamentos/Modulos/ex107.py
@@ -1,7 +1,7 @@
 from Fundamentos.Modulos.funções import aumentar, metade, dobro
 a = int(input("Digite o preço: R$"))
 print('-=' * 20)
-print(f'A metade de R${a} é R${metade(a)}')
-print(f'O dobro de R${a} é R${dobro(a)}')
-print(f'Aumentando 10%, temos R${aumentar(a)}')
+print(f'A metade de R${a} é R${metade(a)} Reais')
+print(f'O dobro de R${a} é R${dobro(a)} reais')
+print(f'Aumentando 10%, temos R${aumentar(a)} reais ')
 print('-=' * 20)
